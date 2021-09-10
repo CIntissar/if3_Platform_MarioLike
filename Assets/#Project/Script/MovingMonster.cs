6a5d3c60bc82f1d94383dc3be24335a7b03c860c
CIntissar <intissar.chajaadine@hotmail.com>
2021-09-10 15:27:48 +0200

    Modification commit

---
+++ Assets/#Project/Script/MovingMonster.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingMonster : Monster {// il hérite de "Monster" le script
+
+    public Vector2 speed = Vector2.zero;  // .zero = (0,0)
+    public bool flipX; 
+    private SpriteRenderer spriteR;
+
+    public float hitRange = 0.1f; 
+    
+    Vector2 start;
+    Vector2 direction;
+        
+
+    void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+        //orientation de l'objet
+        if(speed.x < 0)
+        {
+            spriteR.flipX = true;
+            start = (Vector2)transform.position + Vector2.left * 0.51f;
+            direction = Vector2.left;
+
+        }
+        else if(speed.x > 0)
+        {
+            spriteR.flipX  = false;
+            start = (Vector2)transform.position + Vector2.right * 0.51f;
+            direction = Vector2.right;
+        }
+
+        
+
+        Debug.DrawRay(start, direction * hitRange, Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(start, direction, hitRange); 
+        if(hit.collider != null )
+        {
+            speed.x *= -1;
+        }
+
+        //deplacement
+        Vector2 deplacement = speed * Time.deltaTime;
+        transform.position += (Vector3)deplacement; // casting / caster deplacement dans Vector3
+    }
+}
