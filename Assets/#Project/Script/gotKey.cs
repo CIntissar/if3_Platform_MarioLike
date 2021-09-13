ce4eb2ff7ff51df3a095440fb72bbf7b986c9da5
CIntissar <intissar.chajaadine@hotmail.com>
2021-09-13 13:24:32 +0200

    Because it doesn't save and I made it better

--- Assets/#Project/Script/gotKey.cs
+++ Assets/#Project/Script/gotKey.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class gotKey : MonoBehaviour
 {
-    private SpriteRenderer spriteR;
-
-
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Key"))
         {
-            Destroy(gameObject);
+            Destroy(other.gameObject);
         }
     }
 }
